bf48e656e7a2dc41d7e26eba8217e36bdecf62ac
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2022-05-05 19:53:03 -0400

    Updated the repository code

--- js/script.js
+++ js/script.js
@@ -23,17 +23,21 @@ const navSlide = () => {
 navSlide();
 
 // When the user scrolls the page, execute myFunction
+
 window.onscroll = function () {
   myFunction();
 };
 
 // Get the header
+
 var header = document.getElementById("home");
 
 // Get the offset position of the navbar
+
 var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
